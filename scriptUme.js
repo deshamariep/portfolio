9fdd89bf3f1860343d42ea32c6d3d24044b54346
Desha Poindextere <dmpoinz@gmail.com>
2024-07-12 14:02:12 -0700

    added js

--- scriptUme.js
+++ scriptUme.js
@@ -4,32 +4,27 @@
 
 
     document.addEventListener("DOMContentLoaded", function() {
-        const svgOne = document.getElementById('sOne');
-        const svgTwo = document.getElementById('sTwo');
-        const svgThree = document.getElementById('sThree');
-        const svgFour = document.getElementById('sFour');
-
+        const segments = document.querySelectorAll(".data > div");
+        const windowHeight = window.innerHeight;
+    
+        function isElementInViewport(el) {
+            const rect = el.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.bottom <= windowHeight
+            );
+        }
+    
         function handleScroll() {
-            let scrollPosition = window.scrollY;
-            
-            if (scrollPosition >= 3600) {
-                animateCircle(svgOne, 'sOne', '#GradientColor1', 420); // Adjust parameters as needed
-                animateCircle(svgTwo, 'sTwo', '#GradientColor2', 434); // Adjust parameters as needed
-                animateCircle(svgThree, 'sThree', '#GradientColor3', 293); // Adjust parameters as needed
-                animateCircle(svgFour, 'sFour', '#GradientColor4', 274); // Adjust parameters as needed
-            }
+            segments.forEach((segment, index) => {
+                const svg = segment.querySelector("svg");
+                if (isElementInViewport(segment)) {
+                    svg.classList.add("animate");
+                    window.removeEventListener("scroll", handleScroll); // Remove scroll listener once animation is triggered
+                }
+            });
         }
-
-        function animateCircle(svgElement, id, gradientId, strokeDashOffset) {
-            svgElement.style.animation = `${id} 2s linear forwards`;
-            svgElement.style.stroke = `url(${gradientId})`;
-            svgElement.style.strokeDashoffset = strokeDashOffset;
-        }
-
-        // Initial check on page load
-        handleScroll();
-
-        // Listen for scroll events
+    
         window.addEventListener("scroll", handleScroll);
     });
 }());
