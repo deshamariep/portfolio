874df00db2ddf9de6e44f0e0d475176a95696f4c
Desha Poindextere <dmpoinz@gmail.com>
2024-07-13 17:25:11 -0700

    added js

--- scriptI.js
+++ scriptI.js
@@ -13,12 +13,10 @@
             const gstOffset = 1130;
             const umeOffset = 1730;
 
-            // Reset all case studies to their normal state
             caseStudySpots.classList.remove("active-case-study");
             caseStudyGst.classList.remove("active-case-study");
             caseStudyUme.classList.remove("active-case-study");
 
-            // Determine which case study to activate based on scroll position
             if (scrollPosition >= spotsOffset && scrollPosition < gstOffset) {
                 caseStudySpots.classList.add("active-case-study");
             } else if (scrollPosition >= gstOffset && scrollPosition < umeOffset) {
@@ -28,10 +26,8 @@
             }
         }
 
-        // Initial check on page load
         handleScroll();
-
-        // Listen for scroll events
+        
         window.addEventListener("scroll", handleScroll);
     });
     
@@ -42,23 +38,19 @@
 
         function changeTitle() {
             titleElement.textContent = titles[currentIndex];
-            currentIndex = (currentIndex + 1) % titles.length; // Cycle through titles
+            currentIndex = (currentIndex + 1) % titles.length;
         }
 
-        // Initial call to change title
         changeTitle();
 
-        // Set interval to change title every 3 seconds
-        const intervalId = setInterval(changeTitle, 2500);
+        const intervalId = setInterval(changeTitle, 2000);
 
-        // Stop interval when title reaches "Product Design"
         function stopInterval() {
             if (titleElement.textContent === "Product Designer") {
                 clearInterval(intervalId);
             }
         }
 
-        // Check if title reaches "Product Design" on scroll
         window.addEventListener("scroll", stopInterval);
     });
 }());
