2b6d7b8d1596e9d237a955058dc46d63c1d3b1f2
Desha Poindextere <dmpoinz@gmail.com>
2024-07-15 05:23:10 -0700

    added gst js

--- scriptGst.js
+++ scriptGst.js
@@ -22,10 +22,22 @@
                 const svg = segment.querySelector("svg");
                 if (isElementInViewport(segment)) {
                     svg.classList.add("animate");
+                    startStrokeAnimation(svg); // Start the stroke animation
+                }
+            });
+        }
+    
+        function startStrokeAnimation(svg) {
+            const circles = svg.querySelectorAll("circle");
+            circles.forEach(circle => {
+                const id = circle.id;
+                if (id) {
+                    circle.style.animationPlayState = "running";
                 }
             });
         }
     
         window.addEventListener("scroll", handleScroll);
     });
+    
 }());
