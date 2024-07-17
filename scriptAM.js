8ebec9074b6fd789ecd11a488f568ca8b784c76a
Desha Poindextere <dmpoinz@gmail.com>
2024-07-16 19:22:50 -0700

    added idk

--- scriptAM.js
+++ scriptAM.js
@@ -2,34 +2,13 @@
     'use strict';
     console/log('reading js');
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Select the card
-        const card = document.querySelector('.card');
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     // Select the card
+    //     const card = document.querySelector('.card');
         
-        // Add the class to flip to back
-        card.classList.add('flip-to-back');
-    });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const spotsButton = document.getElementById('spots');
-        spotsButton.addEventListener('click', function() {
-            window.location.href = 'spots.html';
-        });
-    });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const umeButton = document.getElementById('gStreet');
-        umeButton.addEventListener('click', function() {
-            window.location.href = 'ume.html';
-        });
-    });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const gstButton = document.getElementById('gStreet');
-        gstButton.addEventListener('click', function() {
-            window.location.href = 'gStreet.html';
-        });
-    });
+    //     // Add the class to flip to back
+    //     card.classList.add('flip-to-back');
+    // });
 
     
 
