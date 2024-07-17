149b062f7fe12143b5b7b780ec0edb9e3b076575
Desha Poindextere <dmpoinz@gmail.com>
2024-07-17 11:28:40 -0700

    added flip

--- scriptAM.js
+++ scriptAM.js
@@ -2,13 +2,15 @@
     'use strict';
     console/log('reading js');
 
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     // Select the card
-    //     const card = document.querySelector('.card');
-        
-    //     // Add the class to flip to back
-    //     card.classList.add('flip-to-back');
-    // });
+    // Get all .card elements
+    const cards = document.querySelectorAll('.card');
+
+    // Loop through each card and add click event listener
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            card.classList.toggle('flipped');
+        });
+    });
 
     
 
