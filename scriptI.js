7b2d47ffdd08d456d70917d40a1b83012490eab7
Desha Poindextere <dmpoinz@gmail.com>
2025-08-27 18:05:40 -0700

    added css

--- scriptI.js
+++ scriptI.js
@@ -25,6 +25,15 @@
       csInfo.innerHTML = '<div><h1>G Street UX</h1><p>Creating inclusive community spaces from downtown revitalization to vibrant public experiences</p></div><div><img src="images/gStreetImages.png" alt="Images from Davis California G Street, historical arch, team image, current G Street" height="481px" width="939px"></div>';
     });
 
+     // Spots
+     document.getElementById('spotsCS').addEventListener('mouseover', function() {
+      csInfo.innerHTML = '<div><h1>Spots</h1><p>A centralized hub connecting students with housing, roommates, and community.</p></div><div><img src="images/spotsImages.png" alt="Frames from Spots website application" height="497px" width="840px"></div>';
+    });
+
+    // Ume
+    document.getElementById('umeCS').addEventListener('mouseover', function() {
+      csInfo.innerHTML = '<div><h1>UME Tea</h1><p>Transforming marketing strategy through comprehensive analysis and targeted campaign development</p></div><div><img src="images/umeTeaImages.png" alt="Images from UME Tea Marketing Case Study, pie chart segmentation, graph chart customer values, user persona" height="548px" width="821px"></div>';
+    });
 
     // garden.addEventListener('mouseout', function() {
     //     plantInfo.innerHTML = '<h3> <- hover for info</h3>';
