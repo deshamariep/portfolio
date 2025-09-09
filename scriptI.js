e0debd94bb87283594e48505e1513cdbc38c8aa8
Desha Poindextere <dmpoinz@gmail.com>
2025-09-09 00:23:31 -0700

    added id

--- scriptI.js
+++ scriptI.js
@@ -1,4 +1,3 @@
-
 (function(){
   'use strict';
   console.log("reading js");
@@ -26,7 +25,7 @@
 
   // Amplfy
   document.getElementById('bridgegood').addEventListener('mouseover', function() {
-      csInfo.innerHTML = '<div><h1>Amplfy</h1><p>A living network that turns opportunities into results</p></div><div><img src="images/amplfyImages.svg" alt="Frame from Amplfy application" height="475px" width="954px"></div>';
+      csInfo.innerHTML = '<div id="csText"><h1>Amplfy</h1><p>A living network that turns opportunities into results</p></div><div id="csImage"><img src="images/amplfyImages.svg" alt="Frame from Amplfy application" height="475px" width="954px"></div>';
       // garden.style.animationPlayState = 'paused';
   });
   // document.getElementById('bridgegood').addEventListener('mouseout', function() {
@@ -36,22 +35,22 @@
 
   // TAP Series
   document.getElementById('tapSeries').addEventListener('mouseover', function() {
-    csInfo.innerHTML = '<div><h1>TAP Series</h1><p>Designing better interfaces and streamlining 50K+ weekly marketing lead workflow</p></div><div><img src="images/tapSeriesImages.svg" alt="Frames from TAP Series website projects" height="475px" width="867px"></div>';
+    csInfo.innerHTML = '<div id="csText"><h1>TAP Series</h1><p>Designing better interfaces and streamlining 50K+ weekly marketing lead workflow</p></div><div id="csImage"><img src="images/tapSeriesImages.svg" alt="Frames from TAP Series website projects" height="475px" width="867px"></div>';
   });
 
   // GSt
   document.getElementById('gSt').addEventListener('mouseover', function() {
-    csInfo.innerHTML = '<div><h1>G Street UX</h1><p>Creating inclusive community spaces from downtown revitalization to vibrant public experiences</p></div><div><img src="images/gStreetImages.svg" alt="Images from Davis California G Street, historical arch, team image, current G Street" height="475px" width="927px"></div>';
+    csInfo.innerHTML = '<div id="csText"><h1>G Street UX</h1><p>Creating inclusive community spaces from downtown revitalization to vibrant public experiences</p></div><div id="csImage"><img src="images/gStreetImages.svg" alt="Images from Davis California G Street, historical arch, team image, current G Street" height="475px" width="927px"></div>';
   });
 
    // Spots
    document.getElementById('spotsCS').addEventListener('mouseover', function() {
-    csInfo.innerHTML = '<div><h1>Spots</h1><p>A centralized hub connecting students with housing, roommates, and community.</p></div><div><img src="images/spotsImages.svg" alt="Frames from Spots website application" height="475px" width="811px"></div>';
+    csInfo.innerHTML = '<div id="csText"><h1>Spots</h1><p>A centralized hub connecting students with housing, roommates, and community.</p></div><div id="csImage"><img src="images/spotsImages.svg" alt="Frames from Spots website application" height="475px" width="811px"></div>';
   });
 
   // Ume
   document.getElementById('umeCS').addEventListener('mouseover', function() {
-    csInfo.innerHTML = '<div><h1>UME Tea</h1><p>Transforming marketing strategy through comprehensive analysis and targeted campaign development</p></div><div><img src="images/umeTeaImages.svg" alt="Images from UME Tea Marketing Case Study, pie chart segmentation, graph chart customer values, user persona" height="475px" width="712px"></div>';
+    csInfo.innerHTML = '<div id="csText"><h1>UME Tea</h1><p>Transforming marketing strategy through comprehensive analysis and targeted campaign development</p></div><div id="csImage"><img src="images/umeTeaImages.svg" alt="Images from UME Tea Marketing Case Study, pie chart segmentation, graph chart customer values, user persona" height="475px" width="712px"></div>';
   });
 
   // pre-load images
