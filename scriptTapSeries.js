1ac1a9512213a264056995e3e0e27c5f2c3a65b6
Desha Poindextere <dmpoinz@gmail.com>
2025-12-16 19:43:16 -0800

    added index

--- scriptTapSeries.js
+++ scriptTapSeries.js
@@ -11,7 +11,7 @@
           setTimeout(() => {
             const target = document.querySelector(hash);
             if (target) {
-              const headerOffset = 48;   
+              const headerOffset = 88;   
               const y = target.getBoundingClientRect().top + window.pageYOffset - headerOffset;
         
               window.scrollTo({
@@ -31,7 +31,7 @@
         setTimeout(() => {
           const target = document.querySelector(hash);
           if (target) {
-            const headerOffset = 64;
+            const headerOffset = 88;
             const y = target.getBoundingClientRect().top + window.pageYOffset - headerOffset;
       
             window.scrollTo({
