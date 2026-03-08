e90ec7b27fc53d85c958e3ba277853e288203a5f
Desha Poindextere <dmpoinz@gmail.com>
2026-03-08 04:49:34 -0700

    added footer, cursor

--- scriptI.js
+++ scriptI.js
@@ -126,21 +126,21 @@
 
 
   document.addEventListener("DOMContentLoaded", () => {
-      const footer = document.querySelector("footer");
-      const footTitle = document.getElementById("footTitle");
-  
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              footTitle.style.animationPlayState = "running";
-            }
-          });
-        },
-        { threshold: 0.1 }
-      );
-  
-      observer.observe(footer);
+    const footer = document.querySelector("footer");
+    const footTitle = document.getElementById("footTitle");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            footTitle.style.animationPlayState = "running";
+          }
+        });
+      },
+      { threshold: 0.2 } // you can adjust this
+    );
+
+    observer.observe(footer);
   });
 
 
